b1f4ad344e641ec651af83dc52cff11cad1ab79d
gisela ortt <ms.giselaortt@gmail.com>
2022-07-07 13:36:38 -0300

    another attempt on 2 bfs/dfs problems.

--- graph/1469.py
+++ graph/1469.py
@@ -3,48 +3,32 @@
 import numpy as np
 import queue
 
-'''
-def query( graph, numberOfEmployee, ages ):
-    youngestAge = 101
-    myqueue = queue.Queue()
-
-    for i in range( graph.shape[0] ):
-        if( graph[i][numberOfEmployee] == 1 ):
-            myqueue.put(i)
-
-    while( myqueue.empty() == False ):
-        nextManager = myqueue.get()
-        for i in range( graph.shape[0] ):
-            if( graph[i][nextManager] == 1 ):
-                myqueue.put(i)
-                if( ages[i-1] <= youngestAge ):
-                    youngestAge = ages[i-1]
-
-    return youngestAge
-'''
 
 
+def query( graph, employeeIndex, ages ):
+    
 
-
-def query( graph, employeeNumber, ages ):
     youngest = 101
 
     for i in range(  len(graph)  ):
-        if( graph[i][ employeeNumber ] == 1 ):
+        if( graph[i][ employeeIndex ] == 1 ):
             youngest = min( youngest, ages[i-1], query( graph, i, ages ) )
 
     return youngest
 
 
-def swap( graph, firstEmployee, secondEmployee ):
+def swap( firstEmployee, secondEmployee, ages, index ):
+    '''
     for i in range( len(graph) ):
         if( i != firstEmployee and i != secondEmployee ):
             graph[i][ firstEmployee ], graph[i][ secondEmployee ] = graph[i][ secondEmployee ], graph[i][ firstEmployee ]
             graph[ firstEmployee ][i], graph[ secondEmployee ][i] = graph[ secondEmployee ][i], graph[firstEmployee][i]
 
-    graph[ firstEmployee ][ secondEmployee ] = graph[ secondEmployee ][ firstEmployee ]
+    graph[ firstEmployee ][ secondEmployee ], graph[ secondEmployee ][ firstEmployee ] =graph[ secondEmployee ][ firstEmployee ],  graph[ firstEmployee ][ secondEmployee ]
+    '''
 
-    return graph
+    ages[ firstEmployee -1 ], ages[ secondEmployee - 1 ] = ages[ secondEmployee -1 ], ages[ firstEmployee -1  ]
+    index[ firstEmployee ], index[ secondEmployee ] = index[ secondEmployee ], index[ firstEmployee ]
 
 
 if __name__ == '__main__':
@@ -54,11 +38,10 @@ if __name__ == '__main__':
     totalOfEmployee = int(inputs[0])
     totalOfRelationships = int(inputs[1])
     totalOfQuestions = int(inputs[2])
-
     graph = [ [0]*(totalOfEmployee+1) for i in range(totalOfEmployee+1) ]
     #graph = np.zeros(( totalOfEmployee+1, totalOfEmployee+1 ))
-
     ages = [int(element) for element in input().rstrip().split()]
+    index = list(range(totalOfEmployee+1))
 
 
     for i in range(totalOfRelationships):
@@ -72,11 +55,12 @@ if __name__ == '__main__':
         inputs = input().rstrip().split()
 
         if( inputs[0] == 'P' ):
-           answer = query( graph, int(inputs[1]), ages )
-           if( answer == 101 ):
-               print("*")
-           else:
+            indexOfEmployee = index[ int(inputs[1]) ]
+            answer = query( graph, indexOfEmployee, ages )
+            if( answer == 101 ):
+                print("*")
+            else:
                 print(answer)
         
         if( inputs[0] == 'T' ):
-            swap( graph, int(inputs[1]), int(inputs[2]) )
+            swap( int(inputs[1]), int(inputs[2]), ages, index )
---
+++ graph/1928.py
@@ -0,0 +1,52 @@
+#url of the problem:
+#https://www.beecrowd.com.br/judge/en/problems/view/1928
+
+
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+import queue
+
+
+def calculatePath( graph, startingPoint, values ):
+    myqueue = queue.Queue()
+    distances = [-1]*(len(graph)+1)
+    distances[ startingPoint ] = 0
+    myqueue.put( startingPoint )
+    targetValue = values[ startingPoint-1 ]
+
+    answer = -1
+
+    while( not myqueue.empty() ):
+        currentNode = myqueue.get()
+        for i in range(len(graph)):
+            if( graph[ currentNode ][ i ] == 1 and distances[ i ] == -1 ):
+                distances[ i ] = distances[ currentNode ] + 1
+                myqueue.put( i )
+                if( values[i-1] == targetValue ):
+                    answer = distances[ i ]
+                    break
+    print(answer)
+    return answer
+
+
+if __name__ == '__main__':
+    numberOfCards  = int(input().rstrip())
+    cardValues = [ int(value) for value in input().rstrip().split() ]
+    #graph = np.zeros((numberOfCards, numberOfCards), type=int)
+    graph = [ [0]*(numberOfCards+1) for i in range( numberOfCards+1 ) ]
+
+    for i in range(numberOfCards - 1):
+        inputs = input().strip().split()
+        graph[int(inputs[0])][int(inputs[1])] = 1
+        graph[int(inputs[1])][int(inputs[0])] = 1
+
+    totalSum = 0
+    counted = [0]*(int(numberOfCards/2)+1)
+    for i in range( numberOfCards ):
+        if( counted[ cardValues[i] ] == 0 ):
+            totalSum = totalSum + calculatePath( graph, i, cardValues )
+
+    print( totalSum )
+
