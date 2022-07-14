482e819dfe8eb2ca109a230d9bda826f4cc538b8
gisela ortt <ms.giselaortt@gmail.com>
2022-07-14 09:06:15 -0300

    optimized 1928 algorithm but still got a tle

--- graph/1928.py
+++ graph/1928.py
@@ -1,52 +1,66 @@
 #url of the problem:
 #https://www.beecrowd.com.br/judge/en/problems/view/1928
-
-
 # -*- coding: utf-8 -*-
 
+from collections import deque
+import copy
 
-import numpy as np
-import queue
 
+def calculateDistances( graph ):
+    queue = deque()
+    wasVisited = set()
+    startingPoint = 1 # it makes no diference where we start this calculation so i will simply start at zero.
+    queue.append( startingPoint )
+    numberOfNodes = len(graph)
+    distances = { index : dict() for index in range( 1, numberOfNodes+1 ) }
 
-def calculatePath( graph, startingPoint, values ):
-    myqueue = queue.Queue()
-    distances = [-1]*(len(graph)+1)
-    distances[ startingPoint ] = 0
-    myqueue.put( startingPoint )
-    targetValue = values[ startingPoint-1 ]
+    for index in distances.keys():
+        distances[ index ][ index ] = 0
 
-    answer = -1
+    while( len( queue ) > 0 ):
+        currentNode = queue.popleft()
+        if( currentNode in wasVisited ):
+            continue
+        wasVisited.add( currentNode )
+        for nextNode in graph[ currentNode ]:
+            if( nextNode not in wasVisited ):
+                queue.append( nextNode )
+                distances[ nextNode ][ currentNode ] = 1
+                distances[ currentNode ][ nextNode ] = 1
+                for node, distance in distances[ currentNode ].items() :
+                    if( node not in distances[ nextNode ] ):
+                        distances[ nextNode ][ node ] = distances[ node ][ nextNode ] = distnce + 1
 
-    while( not myqueue.empty() ):
-        currentNode = myqueue.get()
-        for i in range(len(graph)):
-            if( graph[ currentNode ][ i ] == 1 and distances[ i ] == -1 ):
-                distances[ i ] = distances[ currentNode ] + 1
-                myqueue.put( i )
-                if( values[i-1] == targetValue ):
-                    answer = distances[ i ]
-                    break
-    return answer
+    return distances
 
 
 if __name__ == '__main__':
     numberOfCards  = int(input().rstrip())
-    cardValues = [ int(value) for value in input().rstrip().split() ]
-    #graph = np.zeros((numberOfCards, numberOfCards), type=int)
-    graph = [ [0]*(numberOfCards+1) for i in range( numberOfCards+1 ) ]
-
-    for i in range(numberOfCards - 1):
-        inputs = input().strip().split()
-        graph[int(inputs[0])][int(inputs[1])] = 1
-        graph[int(inputs[1])][int(inputs[0])] = 1
-
+    cardValues = {}
+    pairsOfPositions = {}
+    graph = { index : list() for index in range( 1, numberOfCards+1 ) }
     totalSum = 0
-    counted = [0]*(int(numberOfCards/2)+1)
-    for i in range( 1, numberOfCards ):
-        if( counted[ cardValues[i-1] ] == 0 ):
-            totalSum = totalSum + calculatePath( graph, i, cardValues )
-            counted[ cardValues[i-1] ] = 1
+
+    for i, value in zip( range( 1,numberOfCards+1 ), input().rstrip().split() ):
+        value = int( value )
+        cardValues[ i ] = int(value)
+        if( value not in pairsOfPositions ):
+            pairsOfPositions[value] = [ i, -1 ]
+        else:
+            pairsOfPositions[value][1] = i
+
+    for i in range(numberOfCards-1):
+        inputs = input().strip().split()
+        firstNode = int( inputs[0] )
+        secondNode = int( inputs[1] )
+        graph[ firstNode ].append( secondNode )
+        graph[ secondNode ].append( firstNode )
+
+    distances = calculateDistances( graph )
+    #print( distances )
+
+    for pair in pairsOfPositions.values() :
+        totalSum = totalSum + distances[ pair[0] ][ pair[1] ]
 
     print( totalSum )
 
