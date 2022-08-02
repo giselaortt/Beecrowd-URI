ac57c581f9818b7c68c23421189d883048aec054
gisela ortt <ms.giselaortt@gmail.com>
2022-08-02 01:43:38 -0300

    coded swap and query functions. started the dfs function but did not finish. changed variables for global.

--- graph/1469/1469.cpp
+++ graph/1469/1469.cpp
@@ -5,13 +5,29 @@
 #include <queue>
 #include <utility>
 #include <bits/stdc++.h>
+#include <set>
 #define N_MAX 500
 
 
 using namespace std;
 
 
-int query( int employee_number ){
+int n, m, instructions;
+int ages[N_MAX];
+int positionOfEmployee[ N_MAX ];
+int employeeAtPosition[ N_MAX ];
+vector<vector<int>> graph;
+vector< set< int > > managersChain;
+vector< pair<int,int> > employees_ages;
+
+
+int query( int employee_index ){
+    int employee_position = positionOfEmployee[ employee_index ];
+    if( managersChain[ employee_position ].empty() )
+        return -1;
+    for( auto it = employees_ages.begin(); it != employees_ages.end(); it++ )
+        if( managersChain[ employee_position ].count( positionOfEmployee[ it->second ] ) )
+            return it->first;
 
 return 0;
 }
@@ -19,25 +35,52 @@ return 0;
 
 void swap( int employee, int other_employee ){
 
-return;
+    //swap its index
+    int temp;
+    temp = positionOfEmployee[ employee ];
+    positionOfEmployee[ employee ] = positionOfEmployee[ other_employee ];
+    positionOfEmployee[ other_employee ] = temp;
+    
+    //swao its positions
+    temp = employeeAtPosition[ employee ];
+    employeeAtPosition[ employee ] = employeeAtPosition[ other_employee ];
+    employeeAtPosition[ other_employee ] = temp;
+
+}
+
+
+void dfs(){
+    // todo: still did not figure it out how to add the manager to every single one of its employees. maybe the graph needs to change for a single directed one...
+    stack<int> filo;
+    filo.push( 0 );
+    int visited[ N_MAX ];
+    // todo: zero the whole visited vector...
+    while( !filo.empty() ){
+        int current = filo.top();
+        filo.pop();
+        //add to visited...
+        visited[ current ] = 1;
+        for( auto it = graph[ current ].begin(); it!= graph[current].end(); it++ ){
+            if( !visited[ *it ] )
+                filo.push( *it );
+        }
+        // é.........
+    }
+
 }
 
 
 int main(){
 
-    int n, m, instructions;
-    int ages[N_MAX];
-    vector<vector<int>> grafo;
-    vector< pair<int,int> > employees_ages;
-
 
     while( cin >> n >> m >> instructions ){
 
-        grafo.clear();
+        graph.clear();
         employees_ages.clear();
+        managersChain.clear();
 
         for(int i=0; i<n; i++)
-            grafo.push_back( vector<int>() );
+            graph.push_back( vector<int>() );
 
         for( int i=0; i<n; i++ )
             cin >> ages[i],
@@ -48,8 +91,8 @@ int main(){
         int x, y;
         for( int i=0; i<m; i++ )
             cin >> x >> y,
-            grafo[x].push_back( y ),
-            grafo[y].push_back( x );
+            graph[x].push_back( y ),
+            graph[y].push_back( x );
 
         char type_of_instruction;
         int employee, other_employee;
@@ -64,8 +107,6 @@ int main(){
         }
     }
 
-    for( auto it = employees_ages.begin(); it!= employees_ages.end(); it++ )
-        cout << it->first<<endl;
 
 return 0;
 }
